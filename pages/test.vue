61f819e8a3882fa229f0515c3d3e28a8c5453ac3
Daniel Schmidt <2wusel@web.de>
2022-02-23 13:07:41 +0100

    infinite aktive

--- pages/test.vue
+++ pages/test.vue
@@ -1,6 +1,6 @@
 <template>
   <body class="bg-gray-100 py-5">
-    <h1 class="bg-white font-black text-7xl p-5">Testarea</h1>
+    <h1 class="bg-red-500 text-white font-black text-7xl p-5">Testarea</h1>
     <div v-for="article in articles" :key="article.slug" class="container mx-auto md:p-36">
         <p class="pb-4 text-yellow-300 bg-gray-900 text-4xl text-center font-black">{{article.title}}</p>
  
@@ -12,23 +12,30 @@
 
     </div>
 
-    <infinite-loading @infinite="infiniteHandler">
-    </infinite-loading>
+    <button @click="sayhi" class="bg-red-300 text-white font-bold p-2">klick!</button>
+
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
 
   </body>
 </template>
 
 <script>
+
+
 export default {
 
-  setup() {
-    function fetchData() {
-      return $content("articles")
-        .limit(3)
-        .sortBy("slug", "desc")
-        .fetch();
+  methods: {
+    sayhi() {
+      alert('say hi');
+    },
+
+    infiniteHandler() {
+      alert('infinite');
     }
+
+
   },
+
 
   async asyncData ({ $content }) {
     const articles = await $content('articles')
