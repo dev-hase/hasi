47fce016e53081388221227706aa67e4931976bf
Daniel Schmidt <2wusel@web.de>
2022-02-23 15:12:31 +0100

    stylus

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,5 @@
 <template>
-  <body class="bg-gray-100 py-5">
+  <body>
     <h1 class="bg-red-500 text-white font-black text-7xl p-5">Testarea</h1>
     <div v-for="article in articles" :key="article.slug" class="container mx-auto md:p-36">
         <p class="pb-4 text-yellow-300 bg-gray-900 text-4xl text-center font-black">{{article.title}}</p>
@@ -12,7 +12,7 @@
 
     </div>
 
-    <button @click="sayhi" class="bg-red-300 text-white font-bold p-2">klick!</button>
+    <button @click="sayhi()" class="bg-red-300 text-white font-bold p-2">klick!</button>
 
     <infinite-loading @infinite="infiniteHandler"></infinite-loading>
 
@@ -24,21 +24,44 @@
 
 export default {
 
+ 
   methods: {
     sayhi() {
       alert('say hi');
+      //this.fetchData;
+
     },
 
-    infiniteHandler() {
-      alert('infinite');
+    infiniteHandler($state) {
+      //alert('infinite');
+
+      setTimeout(async () => {
+        //page.value += 1;
+        let additionalItems = await this.$content("articles")
+          .limit(3)
+          .skip(3)
+          .sortBy("slug", "desc")
+          .fetch();
+
+        alert(additionalItems);
+
+        if (additionalItems.length > 0) {
+          this.articles.value.push(...additionalItems);
+          return{ articles };
+
+          $state.loaded();
+        } else {
+          $state.complete();
+        }
+      }, 500);
     }
 
 
   },
 
-
   async asyncData ({ $content }) {
     const articles = await $content('articles')
+    .limit(2)
     .sortBy("slug", "desc")
     .fetch()
 
@@ -49,3 +72,10 @@ export default {
 }
 </script>
 
+<style lang="stylus" scoped>
+
+body 
+  background #eee
+  padding 2vw
+
+</style>
