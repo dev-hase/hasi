7067a825ec6e6dfaa7726084aef5fca5ce0d2e01
Daniel Schmidt <2wusel@web.de>
2022-02-27 13:27:41 +0100

    precompress

--- components/fetchimage.vue
+++ components/fetchimage.vue
@@ -11,7 +11,7 @@
           fetchFormat="auto"
           quality="auto"
           class="border-4 border-black w-full"
-          width="100%"
+          width="1024px"
         />        
 
     </div>    
